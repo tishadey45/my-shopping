a3b3aa5724ac2e760afdc9dbc642201d3e4d4fb6
Tisha Dey <tishadey2025@outlook.com>
2026-04-08 21:22:32 +0600

    :memo: first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+      
+        </div>
+        <a className="font-bold text-4xl">Shopping Mall</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Product</a>
+          </li>
+           <li>
+            My order
+          </li>
+         
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn bg-indigo-700 text-white">Button</a>
+      </div>
+    </div>
+  );
+}
